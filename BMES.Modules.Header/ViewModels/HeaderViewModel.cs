a7ef8d827314e91106a96664719ebd3d7c8d9b14
Alexey <cbyat0@mail.ru>
2025-11-07 14:50:43 +0300

    header and left menu styling

--- BMES.Modules.Header/ViewModels/HeaderViewModel.cs
+++ BMES.Modules.Header/ViewModels/HeaderViewModel.cs
@@ -8,9 +8,32 @@ namespace BMES.Modules.Header.ViewModels
 {
     public class HeaderViewModel : BindableBase
     {
+        private string _userName;
+        public string UserName
+        {
+            get { return _userName; }
+            set { SetProperty(ref _userName, value); }
+        }
+
+        private string _userRole;
+        public string UserRole
+        {
+            get { return _userRole; }
+            set { SetProperty(ref _userRole, value); }
+        }
+
+        private string _userInitials;
+        public string UserInitials
+        {
+            get { return _userInitials; }
+            set { SetProperty(ref _userInitials, value); }
+        }
+
         public HeaderViewModel()
         {
-
+            UserName = "Иванов Иван Иванович";
+            UserRole = "Исполнительный директор";
+            UserInitials = "ИИ";
         }
     }
 }
