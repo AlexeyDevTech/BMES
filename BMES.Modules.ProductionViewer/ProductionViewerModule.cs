0545fbc7be9877496172cf76dce12be7c5db07a6
Alexey <cbyat0@mail.ru>
2025-11-10 16:12:39 +0300

    add production viewer module part 1

---
+++ BMES.Modules.ProductionViewer/ProductionViewerModule.cs
@@ -0,0 +1,20 @@
+﻿using BMES.Modules.ProductionViewer.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace BMES.Modules.ProductionViewer
+{
+    public class ProductionViewerModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
---
+++ BMES.Modules.ProductionViewer/Views/ProductionViewerView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace BMES.Modules.ProductionViewer.Views
+{
+    /// <summary>
+    /// Interaction logic for ProductionViewerView
+    /// </summary>
+    public partial class ProductionViewerView : UserControl
+    {
+        public ProductionViewerView()
+        {
+            InitializeComponent();
+        }
+    }
+}
