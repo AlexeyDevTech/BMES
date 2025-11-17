68fcad2e0114bc8fbe6a9c520e32a45d82600b19
Alexey <cbyat0@mail.ru>
2025-11-17 14:04:50 +0300

    visual data

--- BMES.Modules.ProductionViewer/ViewModels/ProductionViewerViewModel.cs
+++ BMES.Modules.ProductionViewer/ViewModels/ProductionViewerViewModel.cs
@@ -9,6 +9,28 @@ namespace BMES.Modules.ProductionViewer.ViewModels
 {
     public class ProductionViewerViewModel : BindableBase, INavigationAware
     {
+        private double _outdoorTemperature = -1.2;
+        private double _indoorTemperature = 21.1;
+        private int _tvoHumidity = 34;
+
+        public double OutdoorTemperature
+        {
+            get => _outdoorTemperature;
+            set { SetProperty(ref _outdoorTemperature, value); }
+        }
+
+        public double IndoorTemperature
+        {
+            get => _indoorTemperature;
+            set { SetProperty(ref _indoorTemperature, value); }
+        }
+
+        public int TvoHumidity
+        {
+            get => _tvoHumidity;
+            set { SetProperty(ref _tvoHumidity, value); }
+        }
+
         public ProductionViewerViewModel()
         {
 
